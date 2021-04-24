b8c5dedb286c40ac987b38c2bcdc080baa800af6
unknown <asmak9060@gamil.com>
2021-04-24 22:42:33 +0500

    Lec9 Practice

---
+++ app/src/main/java/com/example/lecture9/VideoActivity.java
@@ -0,0 +1,22 @@
+package com.example.lecture9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        VideoView videoView=findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resources://" + getPackageName() + "/" + R.raw.video);
+        MediaController mediaController=new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+    }
+}
